fa38a7f49633a8a9252f2fd4798fb5bea4c3b7ea
Lorenzo <lore.sarti.93@gmail.com>
2019-04-03 10:35:18 +0200

    Updated Time Module

--- src/includes/time_module.h
+++ src/includes/time_module.h
@@ -2,6 +2,10 @@
 /*** CORE  ***/
 
 void setupTimeManager();	// Setups global variables used by this module.
+
+
+uint8_t setTimer(uint8_t clock, uint8_t value); 	// Set the given timer to a given value
+uint8_t isElapsed(uint8_t clock);					// Return TRUE when the given clock is elapsed. Decrease the timer and return false otherwise
 // ...
 
 
