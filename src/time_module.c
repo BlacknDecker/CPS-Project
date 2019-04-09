c9f0274089ed65bbe187a1f413bab72a3810aae2
Lorenzo <lore.sarti.93@gmail.com>
2019-04-09 11:26:59 +0200

    Added Wait Functionality in time module

--- src/time_module.c
+++ src/time_module.c
@@ -30,6 +30,19 @@ uint8_t isElapsed(uint8_t clock){
 	}
 }
 
+// Wait for a given time. Return TRUE when done, FALSE otherwise
+uint8_t waitTime(uint8_t clock, uint8_t kiloTicks){
+	if(mydata->timer[clock] && mydata->timer[clock] <= kiloTicks){	// If timer has a value > 0 and less than the given timeout means that the timer has already started
+		isElapsed(clock);
+		if(mydata->timer[clock])										// Check timer after decrease!
+			return FALSE;
+		return TRUE;
+	}else{																// Otherwise i have to set my timer
+		setTimer(clock, kiloTicks);
+		return FALSE;
+	}
+}
+
 
 
 // insert functions that exposes services to the other modules here
