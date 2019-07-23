80bd69972c5596e5e1b59b287dc8e9bcb44a890c
SinaAzad <sinam.azad@Gmail.com>
2019-07-23 16:53:57 +0200

    Module fixes added

--- src/includes/color_module.h
+++ src/includes/color_module.h
@@ -10,7 +10,7 @@ void setStableColor(uint8_t rgbColor);									// Reset to a steady color
 uint8_t getColorFromMessage(uint8_t msg);								// Convert a  color message into an rgb color value	
 uint8_t colorToMessage(uint8_t rgbColor);		
 //----New----
-uint8_t colorToNum(uint8_t r_num);							// Convert a color into its message code
+void colorToNum(uint8_t r_num);							// Convert a color into its message code
 
 // ...
 
